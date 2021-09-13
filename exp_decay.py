c0eeb12fdba2f38caa0ea6606d0f4ec737b2bdb6
Simon <simonfj@ifi.uio.no>
2021-09-13 13:21:41 +0200

    added solve method

--- exp_decay.py
+++ exp_decay.py
@@ -1,6 +1,7 @@
-#%%
+
 from scipy.integrate import solve_ivp
 from math import exp
+from numpy import linspace
 
 
 
@@ -13,8 +14,10 @@ class ExponentialDecay():
         dudt = (-self.a)*u
         return dudt
     
-    
-
+    def solve(self, u0, T, dt):
+        interval = linspace(dt,T)
+        res = solve_ivp(self, y0=[u0],t_span=[0,T], t_eval=interval)
+        return res
 
 
 
@@ -23,4 +26,4 @@ class ExponentialDecay():
 if __name__ == "__main__":
     test = ExponentialDecay(0.4)
     print(test.a)
-# %%
+
