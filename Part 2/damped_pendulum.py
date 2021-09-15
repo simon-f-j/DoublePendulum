6e3ead5c43af6883fb64ed6f449b57f62ac47590
Simon <simonfj@ifi.uio.no>
2021-09-15 09:47:34 +0200

    added animation for damped pendulum

--- Part 2/damped_pendulum.py
+++ Part 2/damped_pendulum.py
@@ -4,7 +4,7 @@ from operator import xor
 from scipy.integrate import solve_ivp
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib.animation as animation
 
 class DampenedPendulum(Pendulum_2):
     def __init__(self, L=1, M=1, B=1, g=9.81):
@@ -22,6 +22,11 @@ class DampenedPendulum(Pendulum_2):
         self.__K = None
         self.__vx = None
         self.__vy = None
+        self.__dt = None
+
+
+
+
 
     def __call__(self, t, y):
         theta, omega = y
@@ -32,11 +37,38 @@ class DampenedPendulum(Pendulum_2):
 
         return theta_dot, omega_dot
     
+    def create_animation(self):
+    # Create empty figure
+        fig = plt.figure()
+
+    # Configure figure
+        plt.axis('equal')
+        plt.axis('off')
+        plt.axis((-4, 4, -4, 4))
+        
+       
+        x0 = self.x[0]
+        y0 = self.y[0]
+
+        self.pendulums, = plt.plot([0, x0], [0, y0],'o-', lw=3)
+        self.animation = animation.FuncAnimation(fig,
+                                                self._next_frame,
+                                                frames=range(len(self.x)), 
+                                                repeat=None,
+                                                interval=1000*self.dt, 
+                                                blit=True)
+
+    def _next_frame(self, i):
+        self.pendulums.set_data([0,self.x[i]], [0,self.y[i]])
+        return self.pendulums,
+
+
 
 if __name__=="__main__":
     test = DampenedPendulum(2.7,1)
-    test.solve((45,0),10,0.1,angle="deg")
-    plt.plot(test.x,test.y)
+    test.solve((45,0),100,0.1,angle="deg")
+    test.create_animation()
     plt.show()
+
     
 # %%
